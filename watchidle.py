62d46ddad38f86301e8b10e3aee666abb38b6413
Andy Hebrank <ahebrank@gmail.com>
2016-03-21 21:03:05 -0400

    refactored; untested

--- watchidle.py
+++ watchidle.py
@@ -3,72 +3,75 @@
 
 import os, urllib2, BeautifulSoup, pickle, time
 
-class PlayerStatus():
-  """ 
-  track the status of the players and determine idleness
+class PlayerStatuses:
   """
-  fn = os.path.join(os.path.expanduser('~'), '.playerstatus')
-  idle_minutes = None
+  macs -> status objects
+  provide an interface for the current status of the players
+  """
   statuses = {}
 
-  def __init__(self, idle_minutes = 30):
-    self.idle_minutes = idle_minutes
+  def get(self, mac):
+    if mac in self.statuses:
+      return self.statuses[mac]
+    return None
 
-  def save(self):
-    """
-    save the current statuses
-    """
+  def set(self, mac, status):
+    self.statuses[mac] = status
+
+  def set_status(self, mac, power, play):
+    self.statuses[mac] = {
+      'powerStatus': power,
+      'playStatus': play
+    }
+
+  def set_time(self, mac, tm):
+    if mac in self.statuses:
+      self.statuses[mac]['timestamp'] = tm
+
+  def keys(self):
+    return self.statuses.keys()
+
+  def dump(self, fn):
     for s in self.statuses:
       if not 'timestamp' in self.statuses[s]:
         self.statuses[s]['timestamp'] = time.time()
-    pickle.dump(self.statuses, open(self.fn, 'wb'))
+    pickle.dump(self.statuses, open(fn, 'wb'))
+    return True
 
-  def load(self):
-    """
-    load the pickle
-    """
+  def load(self, fn):
     try:
-      self.statuses = pickle.load(open(self.fn, 'rb'))
+      self.statuses = pickle.load(open(fn, 'rb'))
     except:
       return False
     return True
 
-  def get_macs(self):
-    if self.load():
-      return self.statuses.keys()
-    return None
+  def is_on(self, mac):
+    return self.get_power(mac) == 'on'
 
-  def check_idle(self, statuses):
-    """
-    look at the player statuses, see if any have been idle for a while
-    """
-    to_be_turned_off = []
-    
-    if self.load():
-      # there are some prior statuses
-      now = time.time()
-      for k in statuses:
-        if k not in self.statuses:
-          self.statuses[k] = statuses[k]
+  def is_stopped(self, mac):
+    return self.get_play(mac) == 'stop'
 
-        if ( (statuses[k]['powerStatus'] == 'on') and (self.statuses[k]['powerStatus'] == 'on') and
-          (statuses[k]['playStatus'] == 'stop') and (self.statuses[k]['playStatus'] == 'stop') and
-          ((time.time() - self.statuses[k]['timestamp']) > 60*self.idle_minutes) ):
-            # idle exceeded; return the mac address
-            to_be_turned_off.append(k)
-        elif ( (statuses[k]['powerStatus'] != self.statuses[k]['powerStatus']) or
-          (statuses[k]['playStatus'] != self.statuses[k]['playStatus']) ):
-            # something has changed; update the timestamp
-            statuses[k]['timestamp'] = now
-        else:
-          # no change; keep the old timestamp
-          statuses[k]['timestamp'] = self.statuses[k]['timestamp']
+  def get_power(self, mac):
+    return self.get_status(mac, 'powerStatus')
 
-    self.statuses = statuses
-    self.save()
-    return to_be_turned_off
+  def get_play(self, mac):
+    return self.get_status(mac, 'playStatus')
 
-class SqueezeComm():
+  def get_time(self, mac):
+    return self.get_status(mac, 'timestamp')
+
+  def get_status(self, mac, key):
+    if not key in ['powerStatus', 'playStatus', 'timestamp']:
+      return None
+    if not mac in self.statuses:
+      return None
+    return self.statuses[mac][key]
+
+  def is_idle(self, mac, idle_minutes):
+    return ((time.time() - self.statuses[mac]['timestamp']) > 60*idle_minutes)
+
+
+class SqueezeComm:
   """
   manage communication with the server
   """
@@ -106,27 +109,25 @@ class SqueezeComm():
     html = self.fetch_html(query_params)
     return BeautifulSoup.BeautifulSoup(html)
 
-  def get_player_status(self, mac):
+  def get_player_status_markup(self, mac):
     """
     return current player status given a mac address
     """
     bs = self.get_soup(mac)
-    statuses = bs.findAll('b')
+    return bs.findAll('b')
 
-    # statuses will be play, pause, or stop
-    return {'playStatus' : statuses[0].text.lower(), 
-            'powerStatus': statuses[3].text.lower()}
-
-
-  def statuses(self, macs = None):
+  def check_statuses(self):
     """
     return a dictionary of play status for each player
     """
-    if macs is None:
-      bs = self.get_soup()
-      player_select = bs.find('select', {'name': 'player'})
-      macs = [p['value'] for p in player_select.findAll('option')]
-    return {mac: self.get_player_status(mac) for mac in macs}
+    bs = self.get_soup()
+    player_select = bs.find('select', {'name': 'player'})
+    macs = [p['value'] for p in player_select.findAll('option')]
+    statuses = PlayerStatuses()
+    for mac in macs:
+      status_markup = self.get_player_status_markup(mac)
+      statuses.set_status(mac, power=status_markup[3].text.lower(), play=status_markup[0].text.lower())
+    return statuses
 
   def turn_off(self, mac):
     """
@@ -136,13 +137,50 @@ class SqueezeComm():
     self.fetch_html(query_params)
 
 
+class CheckStatus:
+  """ 
+  handle the logic of checking idleness between last check and current status
+  """
+  status_fn = os.path.join(os.path.expanduser('~'), '.playerstatus')
+
+  def check_idle(self, current, idle_minutes = 30):
+    """
+    look at the player statuses, see if any have been idle for a while
+    """
+    to_be_turned_off = []
+    
+    last = PlayerStatuses()
+    if last.load(self.status_fn):
+      # there are some prior statuses
+      now = time.time()
+      for k in current.keys():
+        if k not in last.keys():
+          continue
+
+        if ( current.is_on(k) and last.is_on(k) and
+          current.is_stopped(k) and last.is_stopped(k) and
+          last.is_idle(k, idle_minutes) ):
+            # idle exceeded; return the mac address
+            to_be_turned_off.append(k)
+        elif ( (current.get_power(k) != last.get_power(k)) or
+          (current.get_play(k) != last.get_play(k)) ):
+            # something has changed; update the timestamp
+            current.set_time(k, now)
+        else:
+          # no change; keep the old timestamp
+          current.set_time(k, last.get_time(k))
+
+    # pickle the current status
+    statuses.dump(self.status_fn)
+    return to_be_turned_off
+
+
 if __name__ == "__main__":
-  PS = PlayerStatus(idle_minutes = 30)
-  macs = PS.get_macs()
+  S = CheckStatus()
 
   SC = SqueezeComm("slug", "9000")
-  statuses = SC.statuses(macs)
-  idle_macs = PS.check_idle(statuses)
+  statuses = SC.check_statuses()
+  idle_macs = S.check_idle(statuses, idle_minutes = 30)
 
   for mac in idle_macs:
     print "Turning off idle %s" % (mac)
